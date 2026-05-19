df5ff16491906f3752e3417b8f1ef75b509c2dd2
Rafael Lopes <80158191+rafaellopespacheco@users.noreply.github.com>
2026-05-19 17:22:11 -0300

    retirado pasta extra de outro projeto

---
+++ database/init.js
@@ -0,0 +1,63 @@
+const db = require('./db')
+
+db.run(`CREATE TABLE IF NOT EXISTS changelogs(
+        id INTEGER PRIMARY KEY,
+        versao TEXT,
+        titulo TEXT,
+        resumo TEXT,
+        descricao TEXT,
+        data DATE
+)`);
+
+db.run(`CREATE TABLE IF NOT EXISTS config(
+        id INTEGER PRIMARY KEY,
+        nome_empresa TEXT        
+)`);
+
+db.run(`CREATE TABLE IF NOT EXISTS login(
+    nome TEXT,
+    email TEXT,
+    senha TEXT
+)`);
+
+db.run(`CREATE TABLE IF NOT EXISTS clientes(
+    id INTEGER PRIMARY KEY,
+    nome TEXT,
+    cpf TEXT,
+    datanasc DATE,
+    numero TEXT,
+    obs TEXT,
+    logradouro TEXT,
+    bairro TEXT,
+    cidade TEXT,
+    uf TEXT,
+    notificacao INTEGER
+)`);
+
+db.run(`CREATE TABLE IF NOT EXISTS pets(
+    id INTEGER PRIMARY KEY,
+    cliente_id INTEGER,
+    nome TEXT,
+    datanasc DATE,
+    status TEXT,
+    especie TEXT,
+    raca TEXT,
+    porte TEXT,
+    tamanhopelo TEXT,
+    peso REAL,
+    sexo TEXT,
+    castrado INTEGER,
+    obs TEXT
+)`);
+
+db.run(`CREATE TABLE IF NOT EXISTS agendamentos(
+    id INTEGER PRIMARY KEY,
+    cliente_id INTEGER,
+    pet_id INTEGER,
+    data DATE,
+    servicos TEXT,
+    valor REAL,
+    pago REAL,
+    status TEXT,
+    taxi REAL
+)`);
