0f0292c0557e1e07d161398fc413e2a364b6d8c6
Rafael Lopes <80158191+rafaellopespacheco@users.noreply.github.com>
2026-06-02 15:32:17 -0300

    feat: adiciona sistema administrativo de changelogs e versionamento automático

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Layout from './components/Layout';
 import Agenda from './pages/Agenda';
 import Atualizacoes from './pages/Atualizacoes';
+import AddAtualizacoes from './pages/AddAtualizacoes';
 import Clientes from './pages/Cliente'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
@@ -13,6 +14,7 @@ function App() {
                     <Route path='/agenda' element={ <Agenda/> }></Route>
                     <Route path='/clientes' element={ <Clientes/> }></Route>
                     <Route path='/atualizacoes' element={ <Atualizacoes/> }></Route>
+                    <Route path='/addatualizacoes' element={ <AddAtualizacoes/> }></Route>
                 </Route>
             </Routes>
         </BrowserRouter>
