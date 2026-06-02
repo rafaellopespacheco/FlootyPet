8b922cb0758716ce3d0d1ca20d2b9605f40ae15b
Rafael Lopes <80158191+rafaellopespacheco@users.noreply.github.com>
2026-06-02 11:18:02 -0300

    feat: Adiciona destaque no link ativo da navegacao na Sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import '../styles/sidebar.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { toast } from 'sonner';
 
 export default function () {
@@ -8,15 +8,15 @@ export default function () {
             <img src='/assets/branding/logo_white.png' alt='Logo da flooty pet'></img>
             <nav>
                 <ul>
-                    <li><Link to='/agenda' className='link'><span className='material-symbols-rounded'>calendar_today</span>Agenda</Link></li>
-                    <li><Link to='/clientes' className='link'><span className='material-symbols-rounded'>group</span>Clientes</Link></li>
+                    <li><NavLink to='/agenda' className='link'><span className='material-symbols-rounded'>calendar_today</span>Agenda</NavLink></li>
+                    <li><NavLink to='/clientes' className='link'><span className='material-symbols-rounded'>group</span>Clientes</NavLink></li>
                     <li><button className='link desativado' onClick={() => toast.warning("Em desenvolvimento.")}><span className='material-symbols-rounded'>home</span>Hospedagem</button></li>
                     <li><button className='link desativado' onClick={() => toast.warning("Em desenvolvimento.")}><span className='material-symbols-rounded'>deployed_code</span>Pacotes</button></li>
                     <li><button className='link desativado' onClick={() => toast.warning("Em desenvolvimento.")}><span className='material-symbols-rounded'>document_search</span>Relatórios</button></li>
                     <li><button className='link desativado' onClick={() => toast.warning("Em desenvolvimento.")}><span className='material-symbols-rounded'>account_balance</span>Financeiro</button></li>
                     <li><button className='link desativado' onClick={() => toast.warning("Em desenvolvimento.")}><span className='material-symbols-rounded'>package_2</span>Estoque</button></li>
                     <li><a href='/configuracao' className='link'><span className='material-symbols-rounded'>settings</span>Configuração</a></li>
-                    <li><Link to='/atualizacoes' className='link'><span className='material-symbols-rounded'>update</span>Atualizações</Link></li>
+                    <li><NavLink to='/atualizacoes' className='link'><span className='material-symbols-rounded'>update</span>Atualizações</NavLink></li>
                 </ul>
             </nav>
             <button type='button' className='link' id='logout'><span className='material-symbols-rounded'>logout</span>Sair</button>
