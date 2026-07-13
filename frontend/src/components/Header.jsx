6a478df8394a0c5b4640eca9725b0ee6058705bf
Rafael Lopes <80158191+rafaellopespacheco@users.noreply.github.com>
2026-07-12 23:18:07 -0300

    Nova página institucional e melhorias na experiência do usuário
    Logout migrado para o componente Sidebar em React, permitindo que as notificações (Toast) permaneçam funcionando corretamente durante o redirecionamento entre páginas.
    
    Removidos os arquivos HTML antigos utilizados pelo frontend. Agora apenas a página de Configuração permanece utilizando o modelo legado, enquanto as demais páginas já utilizam React.
    
    Criada a página "Conheça o Flooty Pet", apresentando informações sobre o sistema, créditos do desenvolvedor e acesso ao portfólio.
    
    O botão "Esqueci minha senha" agora orienta o usuário a entrar em contato com a equipe de suporte e disponibiliza um botão para acesso direto ao WhatsApp.
    
    O ícone de notificações do cabeçalho deixou de ser apenas um elemento visual e passou a funcionar como um botão. Enquanto a Central de Notificações está em desenvolvimento, é exibido um aviso informativo ao usuário.
    
    Realizadas melhorias internas na estrutura do frontend para facilitar a implementação de novos recursos e tornar o código mais organizado.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,8 +2,12 @@ import { useEffect } from 'react';
 import '../styles/header.css';
 import Profile from './Profile';
 import { useLocation } from 'react-router-dom';
+import { toast } from 'sonner';
 
 export default function () {
+    function handleAbrirNotificacao() {
+        toast.warning("A Central de Notificação está sendo desenvolvida.")
+    }
     const location = useLocation();
 
     const titulos = {
@@ -16,12 +20,17 @@ export default function () {
         <header>
             <h1>{titulos[location.pathname]}</h1>
             <div className="mensagens-container">
-                <img src="/assets/agenda/icon_mensagem.png" alt="Icone de sino"></img>
+                <button className='buttonAbrirNotificacao' title='Central de Notificações' onClick={handleAbrirNotificacao}>
+                    <span class="material-symbols-rounded">notifications</span>
+                </button>
             </div>
             <div className="conta-container">
                 <Profile />
-                <img src="https://s2.glbimg.com/yrFgwzwrwxxpO7pJoejZxwxpRx0=/620x620/smart/e.glbimg.com/og/ed/f/original/2022/03/11/mister_mainer.jpeg" alt=""></img>
+                <img
+                    src="https://s2.glbimg.com/yrFgwzwrwxxpO7pJoejZxwxpRx0=/620x620/smart/e.glbimg.com/og/ed/f/original/2022/03/11/mister_mainer.jpeg"
+                    alt=""
+                ></img>
             </div>
         </header>
-    )
+    );
 }
