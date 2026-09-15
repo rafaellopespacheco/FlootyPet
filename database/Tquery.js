bca8cdc2c0b764d02ea0404027f3819cbbda71dd
Rafael Lopes <80158191+rafaellopespacheco@users.noreply.github.com>
2026-09-15 13:27:49 -0300

    refactor: remove arquivos antigos da raiz

--- database/Tquery.js
+++
@@ -1,5 +0,0 @@
-const db = require('./db');
-
-// db.run(`DROP TABLE changelogs`)
-// db.all(`SELECT * FROM agendamentos`, [], (err, rows) => console.log(rows))
-// db.all(`SELECT * FROM servicos`, [], (err, rows) => console.log(rows))
