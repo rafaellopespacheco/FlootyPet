6bdf10c14e51bd3044af91e95c3cd46684e4e06b
Rafael Lopes <80158191+rafaellopespacheco@users.noreply.github.com>
2026-09-16 13:13:29 -0300

    feat(sidebar): adiciona ao clicar na logo é redirecionado a página inicial da agenda (close #12)

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -4,23 +4,58 @@ import { NavLink } from 'react-router-dom';
 export default function Sidebar () {
 
     return (
-        <aside className='sidebar'>
-            <img src='/assets/branding/logo_white.png' alt='Logo da flooty pet'></img>
+        <aside className="sidebar">
+            <NavLink to="/agenda" className="brandinglink">
+                <img
+                    src="/assets/branding/logo_white.png"
+                    alt="Logo da flooty pet"
+                ></img>
+            </NavLink>
             <nav>
                 <ul>
-                    <li><NavLink to='/agenda' className='link'><span className='material-symbols-rounded'>calendar_today</span>Agenda</NavLink></li>
-                    <li><NavLink to='/clientes' className='link'><span className='material-symbols-rounded'>group</span>Clientes</NavLink></li>
-                    {/* <li><button className='link desativado' onClick={() => toast.warning("Em desenvolvimento.")}><span className='material-symbols-rounded'>home</span>Hospedagem</button></li>
-                    <li><button className='link desativado' onClick={() => toast.warning("Em desenvolvimento.")}><span className='material-symbols-rounded'>deployed_code</span>Pacotes</button></li>
-                    <li><button className='link desativado' onClick={() => toast.warning("Em desenvolvimento.")}><span className='material-symbols-rounded'>document_search</span>Relatórios</button></li>
-                    <li><button className='link desativado' onClick={() => toast.warning("Em desenvolvimento.")}><span className='material-symbols-rounded'>account_balance</span>Financeiro</button></li>
-                    <li><button className='link desativado' onClick={() => toast.warning("Em desenvolvimento.")}><span className='material-symbols-rounded'>package_2</span>Estoque</button></li> */}
-                    <li><NavLink to='/config' className='link'><span className='material-symbols-rounded'>settings</span>Configuração</NavLink></li>
-                    <li><a href='/old/configuracao' className='link'><span className='material-symbols-rounded'>instant_mix</span>Antiga Config</a></li>
-                    <li><NavLink to='/atualizacoes' className='link'><span className='material-symbols-rounded'>update</span>Atualizações</NavLink></li>
+                    <li>
+                        <NavLink to="/agenda" className="link">
+                            <span className="material-symbols-rounded">
+                                calendar_today
+                            </span>
+                            Agenda
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/clientes" className="link">
+                            <span className="material-symbols-rounded">
+                                group
+                            </span>
+                            Clientes
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/config" className="link">
+                            <span className="material-symbols-rounded">
+                                settings
+                            </span>
+                            Configuração
+                        </NavLink>
+                    </li>
+                    <li>
+                        <a href="/old/configuracao" className="link">
+                            <span className="material-symbols-rounded">
+                                instant_mix
+                            </span>
+                            Antiga Config
+                        </a>
+                    </li>
+                    <li>
+                        <NavLink to="/atualizacoes" className="link">
+                            <span className="material-symbols-rounded">
+                                update
+                            </span>
+                            Atualizações
+                        </NavLink>
+                    </li>
                 </ul>
             </nav>
-            <p className='creditos'>&copy; Direitos reservados. 2026</p>
+            <p className="creditos">&copy; Direitos reservados. 2026</p>
         </aside>
-    )
+    );
 }
